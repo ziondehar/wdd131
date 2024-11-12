d9a17e7e1d6136972735b1f8313395446d92940c
ziondehar <ziondehar@gmail.com>
2024-11-12 14:49:00 +0800

    updates

---
+++ week02/scripts/bom.js
@@ -0,0 +1,12 @@
+console.log("JavaScript is linked and working!");
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______');
+
+const li = document.createElement('li')
+li.textContent= "new list item";
+
+const deleteButton = document.createElement("deletebutton");
+deleteButton.textContent = "❌"
+li.append(deleteButton);
+list.append(li)
