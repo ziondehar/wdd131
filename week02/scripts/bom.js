c7c79cccebe92ae80b8df53f9ba84741d4f56f09
ziondehar <ziondehar@gmail.com>
2024-11-15 19:52:39 +0800

    done

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,2 +1,30 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
+button.addEventListener('click', () => {
+    const chapter = input.value.trim();
+
+    if (chapter === '') {
+        alert('Please enter a valid book and chapter!');
+        return;
+    }
+
+    const listItem = document.createElement('li');
+    listItem.textContent = chapter;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(listItem);
+    });
+
+    listItem.appendChild(deleteButton);
+    list.appendChild(listItem);
+
+    input.value = '';
+    input.focus();
+});
 
