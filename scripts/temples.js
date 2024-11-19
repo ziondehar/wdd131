96c29087adc5aa5db11f914c608895858a08a9cb
ziondehar <ziondehar@gmail.com>
2024-11-19 19:08:32 +0800

    nearly complete

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,6 @@
 document.getElementById('Year').textContent = new Date().getFullYear();
 document.getElementById('lastmodified').textContent = document.lastModified;
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+  }
