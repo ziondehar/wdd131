bfcd9da8bc900c5815a52a5776c4c53a681c13a3
ziondehar <ziondehar@gmail.com>
2024-11-24 18:02:00 +0800

    Final edits

--- scripts/place.js
+++ scripts/place.js
@@ -1,2 +1,32 @@
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
+
+function calculateWindChillMetric(temperature, windSpeed) {
+    
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const windChill =
+            13.12 +
+            0.6215 * temperature -
+            11.37 * Math.pow(windSpeed, 0.16) +
+            0.3965 * temperature * Math.pow(windSpeed, 0.16);
+        return windChill.toFixed(1); 
+    } else {
+        return "N/A"; 
+    }
+}
+
+
+function updateWindChillMetric() {
+    const tempElement = document.getElementById("temp");
+    const windSpeedElement = document.getElementById("wind-speed");
+    const windChillElement = document.getElementById("wind-chill");
+
+    const temperature = parseFloat(tempElement.textContent); 
+    const windSpeed = parseFloat(windSpeedElement.textContent); 
+
+    const windChill = calculateWindChillMetric(temperature, windSpeed);
+    windChillElement.textContent = windChill + (windChill !== "N/A" ? "°C" : "");
+}
+
+
+updateWindChillMetric();
