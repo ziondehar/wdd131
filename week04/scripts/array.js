96653b27a56c7db5f7e0da607e5eb9522db9d15c
ziondehar <ziondehar@gmail.com>
2024-12-02 01:14:58 +0800

    array

--- week04/scripts/array.js
+++ week04/scripts/array.js
@@ -1,4 +1,7 @@
 let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
 let namesB = names.filter(name => name.charAt(0) === 'B');
 console.log(namesB);
-let namesmap = names.map((name) => names.length);
+let namesmap = names.map(name => name.length);
+console.log(namesmap);
+let namesreduce = names.reduce((total, name) => total + name.length, 0) / names.length;
+console.log(namesreduce)
