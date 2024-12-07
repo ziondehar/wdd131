518089c48e034edfaf0faa5c867f1760a7c41e4b
ziondehar <ziondehar@gmail.com>
2024-12-07 20:04:25 +0800

    js

--- scripts/form.js
+++ scripts/form.js
@@ -1,2 +1,11 @@
 document.getElementById('Year').textContent = new Date().getFullYear();
 document.getElementById('lastmodified').textContent = document.lastModified;
+
+const products = [
+    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 },
+  ];
+  
